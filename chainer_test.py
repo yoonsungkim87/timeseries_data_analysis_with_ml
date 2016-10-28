6d69264c3053dda02a8db3afcba3b684b87df4c5
Yoon-Sung Kim <yoonsung.kim87@gmail.com>
2016-10-28 11:33:53 +0900

    ''

--- chainer_test.py
+++ chainer_test.py
@@ -1,11 +1,10 @@
-def make_artificial_datasets(t_dur = 100, delay = [3,5], prt= False):
+def make_artificial_datasets(t_dur = 100, delay = [10,20,30,40], prt= False):
     import random
     import numpy as np
     
     buffers = [None for _ in range(len(delay))]
     for i in range(len(delay)):
         buffers[i] = [0] * delay[i]
-        
     result = []
     for i in range(t_dur):
         temp_in = [None for _ in range(len(delay))]
@@ -22,8 +21,35 @@ def make_artificial_datasets(t_dur = 100, delay = [3,5], prt= False):
             print('Y: %2d'%temp_out)
     return np.array(result, dtype=np.int32)
 
+def plot_np_array(np_array, t_direction_down=True ,dim=1, hidden_option=False):
+    get_ipython().magic(u'matplotlib inline')
+    import seaborn as sns
+    import pandas as pd
+    
+    data_list = []
+    if t_direction_down:
+        time = np_array.shape[0]
+        cat = np_array.shape[1]
+    else:
+        time = np_array.shape[1]
+        cat = np_array.shape[0]
+    for i in range(cat):
+        for j in range(time):
+            for k in range(dim):
+                if t_direction_down:
+                    data_list.append([np_array[j][i],i,j,k])
+                else:
+                    data_list.append([np_array[i][j],i,j,k])
+    result = pd.DataFrame(data_list,columns = ['data','cat','time','dim'])
+    if hidden_option:
+        sns.plt.ylim(-2,12)
+        sns.set(style='darkgrid')
+    sns.tsplot(data = result, time='time', condition='cat', value = 'data', unit='dim')
+    if hidden_option:
+        return result
+    
 def main():
-    pass
+    plot_np_array(make_artificial_datasets())
 
 if __name__ == '__main__':
     main()
