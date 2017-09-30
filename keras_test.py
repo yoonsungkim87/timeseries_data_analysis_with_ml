9624507b48372161afb7fd87ae09a18a9a5d55ee
Yoon-Sung Kim <yoonsungkim87@users.noreply.github.com>
2017-09-30 13:49:21 +0900

    Update keras_test.py

--- keras_test.py
+++ keras_test.py
@@ -5,11 +5,9 @@ import seaborn as sns
 import pandas as pd
 from sklearn import preprocessing
 
+
 def make_artificial_datasets(duration = 100, delay = [3,5], hidden = 0):
-    x1_buffer = [0] * delay[0]
-    x2_buffer = [0] * delay[1]
-    X_dataset = []
-    y_dataset = []
+    x1_buffer, x2_buffer, X_dataset, y_dataset = [0] * delay[0] , [0] * delay[1], [], []
     for i in range(duration):
         a = random.randrange(1,10)
         b = random.randrange(1,10)
@@ -28,8 +26,8 @@ def make_artificial_datasets(duration = 100, delay = [3,5], hidden = 0):
         y_array = np.array(y_dataset)
     return X_array, y_array
 
+
 def np_array_to_plot(array, axis = 0,dim =1, hidden_key = 0):
-    
     data_list = []
     if axis:
         time = array.shape[0]
@@ -38,7 +36,6 @@ def np_array_to_plot(array, axis = 0,dim =1, hidden_key = 0):
         time = array.shape[1]
         cat = array.shape[0]
     #print cat, time
-    
     for i in range(cat):
         for j in range(time):
             for k in range(dim):
@@ -52,39 +49,37 @@ def np_array_to_plot(array, axis = 0,dim =1, hidden_key = 0):
         sns.plt.ylim(-2,12)
         sns.set(style='darkgrid')
     sns.tsplot(data = result, time='time', condition='cat', value = 'data', unit='dim')
-    
     if hidden_key:
         return result
     
-def readFile(path="./COP_Data.csv", rStart = 2, cStart=2, in_out_border = 54):
+    
+def readFile(path, row_start, column_start, output_border_start):
     with open(path) as f:
         content = f.readlines()
-    
     content = [a[0].split(",")for a in [x.strip().split("\n") for x in content]]
     np_content = np.array(content)
-    np_content = np_content[rStart-1:].transpose()[cStart-1:].transpose()
+    np_content = np_content[row_start-1:].transpose()[column_start-1:].transpose()
     np_content = np.asarray(np_content, dtype=np.float32)
-        
-    return np_content[:,:in_out_border], np_content[:,in_out_border:]
+    return np_content[:,:output_border_start-1], np_content[:,output_border_start-1:]
+
     
 def main():
-    X, Y = make_artificial_datasets(duration = 1000)
-    np_array_to_plot(np.array([X.transpose()[0],X.transpose()[1], Y.transpose()[0]])[:,:50])
+    X, Y = readFile(path="./COP_Data.csv", row_start=2, column_start=2, output_border_start=55)
+    x_dim = X.shape[1]
+    y_dim = Y.shape[1]
     X = preprocessing.scale(X)
     Y = preprocessing.scale(Y)
     time = 100
-    x_dim = 2
-    y_dim = 1
-
-    X = X.reshape(-1,time,x_dim)
-    Y = Y.reshape(-1,time,y_dim)
+    cutoff = X.shape[0] / time
+    X = X[:cutoff*time,:].reshape(-1,time,x_dim)
+    Y = Y[:cutoff*time,:].reshape(-1,time,y_dim)
     samples = X.shape[0]
     from keras.models import Sequential
     from keras.layers.core import Dense, Activation
     from keras.layers.wrappers import TimeDistributed
     from keras.layers.recurrent import LSTM
     #from keras.optimizers import SGD
-    hidden = 10
+    hidden = 100
     model = Sequential()
     model.add(LSTM(output_dim=hidden, return_sequences=True, stateful=True, init='uniform', 
                    batch_input_shape=(samples, time, x_dim)))
@@ -95,7 +90,7 @@ def main():
     #sgd = SGD(lr=0.1, decay=1e-4, momentum=0.2, nesterov=True)
     #model.compile(loss='mean_squared_error', optimizer=sgd)
     model.fit(X, Y, batch_size=samples, nb_epoch=1000)
-    X_test, Y_test_origin = make_artificial_datasets(duration = 1000, hidden = 1)
+    X_test, Y_test_origin = X[-time:,:].reshape(-1,time,x_dim), Y[-time:,:].reshape(-1,time,y_dim)
     X_test = preprocessing.scale(X_test)
     Y_test = preprocessing.scale(Y_test_origin)
     X_test = X_test.reshape(-1,time,x_dim)
